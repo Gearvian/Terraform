b0c071d9010e3b17f8a4150174bd567b97c96967
Ahmedtr <80450818+Gearvian@users.noreply.github.com>
2024-10-29 17:21:51 +0300

    Delete key.tf

--- key.tf
+++
@@ -1,8 +0,0 @@
-# Create the Key
-
-resource "alicloud_ecs_key_pair" "http" {
-  key_pair_name = "httpkey"
-  key_file      = "httpkey.pem"
-}
-
-#--------------------------------------------------------------------------------------------------------------------------
